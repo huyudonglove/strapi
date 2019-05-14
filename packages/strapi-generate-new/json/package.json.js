e13076967dbc5ac82b8e31e7a24c84f0aedcccd9
Alexandre Bodin <bodin.alex@gmail.com>
2019-05-14 15:48:13 +0200

    Use yarn if installed on strapi new.

--- packages/strapi-generate-new/json/package.json.js
+++ packages/strapi-generate-new/json/package.json.js
@@ -7,8 +7,6 @@
 // Public node modules.
 const _ = require('lodash');
 
-const { packageManager } = require('strapi-utils');
-
 /**
  * Expose main package JSON of the application
  * with basic info, dependencies, etc.
@@ -16,71 +14,79 @@ const { packageManager } = require('strapi-utils');
 
 module.exports = scope => {
   const cliPkg = scope.strapiPackageJSON || {};
-  // Store the package manager info into the package.json
-  const pkgManager = packageManager.isStrapiInstalledWithNPM() ? 'npm' : 'yarn';
 
   // Let us install additional dependencies on a specific version.
   // Ex: it allows us to install the right version of knex.
-  const additionalsDependencies = _.isArray(scope.additionalsDependencies) ?
-    scope.additionalsDependencies.reduce((acc, current) => {
-      const pkg = current.split('@');
-      const name = pkg[0];
-      const version = pkg[1] || 'latest';
+  const additionalsDependencies = _.isArray(scope.additionalsDependencies)
+    ? scope.additionalsDependencies.reduce((acc, current) => {
+        const pkg = current.split('@');
+        const name = pkg[0];
+        const version = pkg[1] || 'latest';
 
-      acc[name] = name.indexOf('strapi') !== -1 ? getDependencyVersion(cliPkg, 'strapi') : version;
+        acc[name] =
+          name.indexOf('strapi') !== -1
+            ? getDependencyVersion(cliPkg, 'strapi')
+            : version;
 
-      return acc;
-    }, {}) : {};
+        return acc;
+      }, {})
+    : {};
 
   // Finally, return the JSON.
   return _.merge(scope.appPackageJSON || {}, {
-    'name': scope.name,
-    'private': true,
-    'version': '0.1.0',
-    'description': 'A Strapi application.',
-    'main': './server.js',
-    'scripts': {
-      'dev': 'strapi dev',
-      'start': 'strapi start',
-      'build': 'strapi build',
-      'strapi': 'strapi', // Allow to use `npm run strapi` CLI,
-      'lint': 'eslint api/**/*.js config/**/*.js plugins/**/*.js'
+    name: scope.name,
+    private: true,
+    version: '0.1.0',
+    description: 'A Strapi application.',
+    main: './server.js',
+    scripts: {
+      dev: 'strapi dev',
+      start: 'strapi start',
+      build: 'strapi build',
+      strapi: 'strapi', // Allow to use `npm run strapi` CLI,
+      lint: 'eslint api/**/*.js config/**/*.js plugins/**/*.js',
     },
-    'devDependencies': {
+    devDependencies: {
       'babel-eslint': '^7.1.1',
-      'eslint': '^4.19.1',
+      eslint: '^4.19.1',
       'eslint-config-airbnb': '^13.0.0',
       'eslint-plugin-import': '^2.11.0',
-      'eslint-plugin-react': '^7.7.0'
+      'eslint-plugin-react': '^7.7.0',
     },
-    'dependencies': Object.assign({}, {
-      'lodash': '^4.17.5',
-      'strapi': getDependencyVersion(cliPkg, 'strapi'),
-      'strapi-admin': getDependencyVersion(cliPkg, 'strapi'),
-      'strapi-utils': getDependencyVersion(cliPkg, 'strapi'),
-      [scope.client.connector]: getDependencyVersion(cliPkg, 'strapi'),
-    }, additionalsDependencies, {
-      [scope.client.module]: scope.client.version
-    }),
-    'author': {
-      'name': scope.author || 'A Strapi developer',
-      'email': scope.email || '',
-      'url': scope.website || ''
+    dependencies: Object.assign(
+      {},
+      {
+        lodash: '^4.17.5',
+        strapi: getDependencyVersion(cliPkg, 'strapi'),
+        'strapi-admin': getDependencyVersion(cliPkg, 'strapi'),
+        'strapi-utils': getDependencyVersion(cliPkg, 'strapi'),
+        [scope.client.connector]: getDependencyVersion(cliPkg, 'strapi'),
+      },
+      additionalsDependencies,
+      {
+        [scope.client.module]: scope.client.version,
+      }
+    ),
+    author: {
+      name: scope.author || 'A Strapi developer',
+      email: scope.email || '',
+      url: scope.website || '',
     },
-    'maintainers': [{
-      'name': scope.author || 'A Strapi developer',
-      'email': scope.email || '',
-      'url': scope.website || ''
-    }],
-    'strapi': {
-      'packageManager': pkgManager,
-      'uuid': scope.uuid
+    maintainers: [
+      {
+        name: scope.author || 'A Strapi developer',
+        email: scope.email || '',
+        url: scope.website || '',
+      },
+    ],
+    strapi: {
+      uuid: scope.uuid,
     },
-    'engines': {
-      "node": ">= 10.0.0",
-      "npm": ">= 6.0.0"
+    engines: {
+      node: '>= 10.0.0',
+      npm: '>= 6.0.0',
     },
-    'license': scope.license || 'MIT'
+    license: scope.license || 'MIT',
   });
 };
 
@@ -89,5 +95,7 @@ module.exports = scope => {
  */
 
 function getDependencyVersion(packageJSON, module) {
-  return module === packageJSON.name ? packageJSON.version : packageJSON.dependencies && packageJSON.dependencies[module];
+  return module === packageJSON.name
+    ? packageJSON.version
+    : packageJSON.dependencies && packageJSON.dependencies[module];
 }
